afbedc3ae7c4bc55d8579f90e2028a985035d992
Hafidh Kurniadi <hafidhjulian@MacBook-Pro-Hafidh.local>
2019-01-22 11:37:15 +0700

    Initial Commit

---
+++ Coba App1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coba App1
+//
+//  Created by Hafidh Julian Kurniadi on 22/01/19.
+//  Copyright © 2019 Hafidh Julian Kurniadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Coba App1Tests/Coba_App1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Coba_App1Tests.swift
+//  Coba App1Tests
+//
+//  Created by Hafidh Julian Kurniadi on 22/01/19.
+//  Copyright © 2019 Hafidh Julian Kurniadi. All rights reserved.
+//
+
+import XCTest
+@testable import Coba_App1
+
+class Coba_App1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
